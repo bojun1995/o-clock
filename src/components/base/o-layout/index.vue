d67cb4651828955b26748eeb6c9ffa7b86be10d7
bojun1995 <bojun1995@foxmail.com>
2021-03-26 22:10:11 +0800

    【修改】scss主题

--- src/components/base/o-layout/index.vue
+++ src/components/base/o-layout/index.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="layout-container">
-    <date-time></date-time>
-    <button @click="onThemeChange('light')">light</button>
-    <button @click="onThemeChange('dark')">dark</button>
+    <o-clock></o-clock>
   </div>
 </template>
 
 <script setup>
 // comp
-import DateTime from '@/components/business/date-time/index.vue'
-import { inject } from 'vue'
-
-const util = inject('$util')
-
-function onThemeChange(theme) {
-  util.theme.setTheme(theme)
-  console.log(util.theme.getTheme())
-}
-
+import OClock from '@/views/o-clock/index.vue'
 </script>
 
 <style lang="scss" rel="stylesheet/scss" type="text/scss" scoped>
@@ -25,5 +14,6 @@ function onThemeChange(theme) {
 
 .layout-container {
   @include background_color('background-color_0');
+  @include color('color_0');
 }
 </style>
---
+++ src/views/o-clock/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <date-time></date-time>
+</template>
+
+<script setup>
+import DateTime from '@/components/business/date-time/index.vue'
+import { reactive, inject } from 'vue'
+
+
+const util = inject('$util')
+
+function onThemeChange(theme) {
+  util.theme.setTheme(theme)
+}
+
+
+</script>
+
+<style lang="scss" rel="stylesheet/scss" type="text/scss" scoped>
+</style>
